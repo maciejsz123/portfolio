dea4022d3e4d5d9805fb6a1aecc4013618bb95e9
maciej <maciejsz0@o2.pl>
2022-03-01 18:37:48 +0100

    Updates

---
+++ precache-manifest.d843743b8b91414380ed500954379855.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc7ebb11c62ae8212d33bd10e961e14",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "30ae1fa9fad86e2ed3fe",
+    "url": "/portfolio/static/css/main.3f9d179b.chunk.css"
+  },
+  {
+    "revision": "e70ebc8bf399bfc149ed",
+    "url": "/portfolio/static/js/2.d697f4db.chunk.js"
+  },
+  {
+    "revision": "30ae1fa9fad86e2ed3fe",
+    "url": "/portfolio/static/js/main.753282cd.chunk.js"
+  },
+  {
+    "revision": "add6ff81d93680170b15",
+    "url": "/portfolio/static/js/runtime-main.2601f1e9.js"
+  },
+  {
+    "revision": "091e69cacd44a5b2be3f422bb0607638",
+    "url": "/portfolio/static/media/callendar.091e69ca.PNG"
+  },
+  {
+    "revision": "be0603d6399b079f1511410e936b8a65",
+    "url": "/portfolio/static/media/clock.be0603d6.PNG"
+  },
+  {
+    "revision": "9f925998fc1eda1be3e6c9f3a6bb35bd",
+    "url": "/portfolio/static/media/hangman.9f925998.PNG"
+  },
+  {
+    "revision": "ac6c99a0287acee693da74b22d5c1559",
+    "url": "/portfolio/static/media/main1.ac6c99a0.jpg"
+  },
+  {
+    "revision": "ddd598334ee9f2b67163df36e0edd02d",
+    "url": "/portfolio/static/media/masterCard.ddd59833.jpg"
+  },
+  {
+    "revision": "f547fafa9bf01645d726cb85c8fc2499",
+    "url": "/portfolio/static/media/realEstate.f547fafa.jpg"
+  }
+]);
